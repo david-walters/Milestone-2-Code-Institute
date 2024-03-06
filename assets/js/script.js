fc87723038639d925ed218e7ac2af7937074850b
david-walters <dawudwalters@gmail.com>
2024-03-06 21:46:46 +0000

    Create a questions array with the question image, answer image, and aswer buttons' text with only one being correct.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ let username = document.getElementById('username');
 
 
 // The start button begins the game and stores the username value. If the username value is empty 
-// then the function will not execute.
+// then the function will not execute and a warning will be displayed to the user.
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault();
@@ -21,5 +21,111 @@ startBtn.addEventListener('click', (event) => {
 
     questionContainer.classList.remove('d-none');
     welcomePopup.classList.add('d-none');
-    username = document.getElementById('username').value;
+    username = username.value;
 });
+
+
+// Questions array containing the question image(image1), correct answer image(image2), and answers for the buttons.
+
+const questions = [
+    {
+        image1: "url(assets/images/answer-images/elephant-poo.webp)",
+        image2: "url(assets/images/answer-images/elephant.webp)",
+        answers: [
+            {text: "Crocodile", correct: false},
+            {text: "Elephant", correct: true},
+            {text: "Dung Beatle", correct: false},
+            {text: "Horse", correct: false}
+        ]
+    },
+    {
+        image1: "url(assets/images/answer-images/sheep-poo.webp)",
+        image2: "url(assets/images/answer-images/sheep.webp)",
+        answers: [
+            {text: "Parrot", correct: false},
+            {text: "Zebra", correct: false},
+            {text: "Bull", correct: false},
+            {text: "Sheep", correct: true}
+        ]
+    },
+    {
+        image1: "url(assets/images/answer-images/bat-poo.webp)",
+        image2: "url(assets/images/answer-images/bat.webp)",
+        answers: [
+            {text: "Rabbit", correct: false},
+            {text: "Mouse", correct: false},
+            {text: "Bat", correct: true},
+            {text: "Budgie", correct: false}
+        ]
+    },
+    {
+        image1: "url(assets/images/answer-images/whale-poo.webp)",
+        image2: "url(assets/images/answer-images/whale.webp)",
+        answers: [
+            {text: "Whale", correct: true},
+            {text: "Shark", correct: false},
+            {text: "Dolphin", correct: false},
+            {text: "Sea Lion", correct: false}
+        ]
+    },
+    {
+        image1: "url(assets/images/answer-images/barn-owl-poo.webp)",
+        image2: "url(assets/images/answer-images/barn-owl.webp)",
+        answers: [
+            {text: "Badger", correct: false},
+            {text: "Barn Owl", correct: true},
+            {text: "Kangaroo", correct: false},
+            {text: "Anaconda", correct: false}
+        ]
+    },
+    {
+        image1: "url(assets/images/answer-images/seagull-poo.webp)",
+        image2: "url(assets/images/answer-images/seagull.webp)",
+        answers: [
+            {text: "Eagle", correct: false},
+            {text: "Cat", correct: false},
+            {text: "Seagull", correct: true},
+            {text: "Penguine", correct: false}
+        ]
+    },
+    {
+        image1: "url(assets/images/answer-images/toad-poo.webp)",
+        image2: "url(assets/images/answer-images/toad.webp)",
+        answers: [
+            {text: "Toad", correct: true},
+            {text: "Slug", correct: false},
+            {text: "Hedgehog", correct: false},
+            {text: "Fox", correct: false}
+        ]
+    },
+    {
+        image1: "url(assets/images/answer-images/cow-poo.webp)",
+        image2: "url(assets/images/answer-images/cow.webp)",
+        answers: [
+            {text: "Tiger", correct: false},
+            {text: "Pony", correct: false},
+            {text: "Bear", correct: false},
+            {text: "Cow", correct: true}
+        ]
+    },
+    {
+        image1: "url(assets/images/answer-images/crocodile-poo.webp)",
+        image2: "url(assets/images/answer-images/crocodile.webp)",
+        answers: [
+            {text: "Crocodile", correct: true},
+            {text: "Giraffe", correct: false},
+            {text: "Leopard", correct: false},
+            {text: "Squirrel", correct: false}
+        ]
+    },
+    {
+        image1: "url(assets/images/answer-images/caterpillar-poo.webp)",
+        image2: "url(assets/images/answer-images/caterpillar.webp)",
+        answers: [
+            {text: "Bullet Ant", correct: false},
+            {text: "Tarantula", correct: false},
+            {text: "Bumble Bee", correct: false},
+            {text: "Caterpillar", correct: true}
+        ]
+    },
+];
