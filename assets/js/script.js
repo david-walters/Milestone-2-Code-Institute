c84d9b9632a6bdfc2c8fce61f205ea39f8301437
david-walters <dawudwalters@gmail.com>
2024-03-06 00:34:07 +0000

    Add an if statement that stops the start button function when name field is empty
    This if statment adds a class that makes the input border red and adds a class that causes '* Please enter your name' to appear in red under the input field.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,21 @@ let questionContainer = document.getElementById('question-container');
 let startBtn = document.querySelector('button[type="submit"]');
 let imageContainer = document.getElementById('image-container');
 let nextBtn = document.getElementById('next-btn');
-let username;
+let labelElement = document.querySelector('label');
+let username = document.getElementById('username');
+
+
+// The start button begins the game and stores the username value. If the username value is empty 
+// then the function will not execute.
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault();
+
+    if (document.getElementById('username').value.trim() === '') {
+        username.style.border = '2px solid red';
+        labelElement.classList.add('attention');
+        return;
+    }
 
     questionContainer.classList.remove('d-none');
     welcomePopup.classList.add('d-none');
